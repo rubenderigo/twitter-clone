ef7af7b99c8a2051bdd0c940274658459c155544
Ruben Derigo <rm.derigo@gmail.com>
2024-02-11 18:17:07 -0300

    feat(post-form): reset status when create post

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -1,36 +1,30 @@
+'use client'
+
 import { Avatar } from '@nextui-org/react'
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
-import { revalidatePath } from 'next/cache'
-import { cookies } from 'next/headers'
-import { ComposePostTextArea } from './compose-post-textarea'
 import { ComposePostButton } from './compose-post-button'
+import { addPost } from '../actions'
+import { useRef } from 'react'
 
 export function ComposePost({ userAvatarUrl }: { userAvatarUrl: string }) {
-  const addPost = async (formData: FormData) => {
-    'use server'
-
-    const content = formData.get('content')
-
-    if (content === null) return
-
-    const supabase = createServerActionClient({ cookies })
-    const {
-      data: { user }
-    } = await supabase.auth.getUser()
-    if (user === null) return
-
-    await supabase.from('posts').insert({ content, user_id: user.id })
-    revalidatePath('/')
-  }
+  const formRef = useRef<HTMLFormElement>(null)
 
   return (
     <form
-      action={addPost}
+      ref={formRef}
+      action={async (formData) => {
+        await addPost(formData)
+        formRef.current?.reset()
+      }}
       className="flex flex-row p-3 border-b border-white/20 gap-2"
     >
       <Avatar radius="full" size="md" src={userAvatarUrl} />
       <div className="flex flex-1 flex-col gap-y-4">
-        <ComposePostTextArea />
+        <textarea
+          name="content"
+          rows={4}
+          className="w-full text-xl bg-black placeholder-gray-500 p-2"
+          placeholder="¡¿Qué está pasando?!"
+        />
         <ComposePostButton />
       </div>
     </form>
